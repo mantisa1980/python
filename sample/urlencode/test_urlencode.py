c1f4fbe1906e0d6d1b9eeaa37676db55d906872e
duyhsieh <mantisa1980@gmail.com>
2017-10-23 15:54:40 +0800

    add urlencode_plus sample

--- sample/urlencode/test_urlencode.py
+++ sample/urlencode/test_urlencode.py
@@ -20,10 +20,18 @@ ASCII control characters, Non-ASCII characters (by unicode encoding), Reserved c
 
 import urllib
 
+# Replace special characters in string using the %xx escape. Letters, digits, and the characters '_.-' are never quoted. By default, this function is intended for quoting the path section of the URL. The optional safe parameter specifies additional characters that should not be quoted — its default value is '/'.
 
 a = "http://abc.com?x=John Carner&y='AAA\"BBB+CCC%DDD"
 
 x = urllib.quote(a)
+
 print "encode of {} is {}".format(a,x)
 
 print "decode of {} is {}".format(x, urllib.unquote(x))
+
+# Like quote(), but also replaces spaces by plus signs, as required for quoting HTML form values when building up a query string to go into a URL. Plus signs in the original string are escaped unless they are included in safe. It also does not have safe default to '/'.
+x = urllib.quote_plus(a)
+print "plus encode of {} is {}".format(a,x)
+
+print "plus decode of {} is {}".format(x, urllib.unquote_plus(x))
