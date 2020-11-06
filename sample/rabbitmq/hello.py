361cf0a36f1fe48b22eff02c4af44236cd51f230
duyhsieh <duyhsieh@igs.com.tw>
2020-11-06 22:02:20 +0800

    add rabbitmq

--- sample/rabbitmq/hello.py
+++ sample/rabbitmq/hello.py
@@ -1,31 +1,20 @@
 import gevent
 from gevent import monkey; monkey.patch_all()
 import pika
+import sys
 
-def callback(ch, method, properties, body):
-    print("channel {},method:{}, properties:{},body:{}".format(ch, method, properties, body))
+def callback1(ch, method, properties, body):
+    #print("Consumer 1: channel {},method:{}, properties:{},body:{}".format(ch, method, properties, body))
+    print("Consumer 1: msg:{}".format(body))
 
-def producer_loop():
-    connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
-    channel = connection.channel()
-    while True:
-        print "Producer publish"
-        channel.basic_publish(exchange='',
-                              routing_key='hello',
-                              body='Hello World!')
-        print(" [x] Sent 'Hello World!'")
-        gevent.sleep(1)
+def callback2(ch, method, properties, body):
+    #print("Consumer 2: channel {},method:{}, properties:{},body:{}".format(ch, method, properties, body))
+    print("Consumer 2: msg:{}".format(body))
 
-def consumer_loop():
-    connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
-    channel = connection.channel()
-    channel.basic_consume(queue='hello',
-                      auto_ack=True,
-                      on_message_callback=callback)
-
-    channel.start_consuming()
-    while True:
-        gevent.sleep(1)
+handlers = {
+    1:callback1,
+    2:callback2,
+}
 
 def init():
     connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
@@ -33,12 +22,65 @@ def init():
     channel.queue_declare(queue='hello')
     connection.close()
 
+def producer_loop():
+    connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+    channel = connection.channel()
+    counter = 1
+    while True:
+        for i in range(0, 5):
+            body = "Hello {}".format(counter)
+            counter+=1
+            channel.basic_publish(exchange='',
+                                  routing_key='hello',
+                                  body=body)
+        #print(" [x] Sent 'Hello World!'")
+        gevent.sleep(1)
+
+def consumer_loop(no, auto_ack=False):
+    connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+    channel = connection.channel()
+    #channel.basic_consume(queue='hello', auto_ack=True, on_message_callback=handlers[no])
+    channel.basic_consume(queue='hello', auto_ack=auto_ack, on_message_callback=handlers[no])
+    channel.start_consuming()
+    while True:
+        gevent.sleep(1)
+
+def produce_sample():
+    connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+    channel = connection.channel()
+    counter = 1
+    for i in range(0, 50):
+        body = "Hello {}".format(counter)
+        counter+=1
+        channel.basic_publish(exchange='',
+                              routing_key='hello',
+                              body=body)
+
+def consume_sample(auto_ack=False):
+    connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+    channel = connection.channel()
+    channel.basic_consume(queue='hello', auto_ack=auto_ack, on_message_callback=callback1)
+    channel.start_consuming()
+    while True:
+        gevent.sleep(1)
+
+def persist_demo():
+    auto_ack = True # queue message flooding test
+    init()
+    produce_sample()
+    consume_sample(auto_ack=auto_ack)
+    while True:
+        gevent.sleep(1)
+
 def main():
+    auto_ack = True
     init()
     gevent.spawn(producer_loop)
-    gevent.spawn(consumer_loop)
+    gevent.spawn(consumer_loop, no=1, auto_ack=auto_ack)
+    gevent.spawn(consumer_loop, no=2, auto_ack=auto_ack)
     while True:
         gevent.sleep(1)
 
 if __name__ == "__main__":
-    main()
+    #main()
+    persist_demo()
